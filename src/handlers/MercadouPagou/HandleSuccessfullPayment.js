2bc76e08e5c34eb2b015221e2d262817b3a4e453
Omarx32 <rmarin.0812@gmail.com>
2023-10-18 20:40:58 -0430

    euu

--- src/handlers/MercadouPagou/HandleSuccessfullPayment.js
+++ src/handlers/MercadouPagou/HandleSuccessfullPayment.js
@@ -2,7 +2,7 @@
 
 const handleSuccessfulPayment = (req, res) => {
   try {
-    res.redirect("https://pf-2-lavenganza-front-n8elcrgbq-migorriti.vercel.app/");
+    res.redirect("https://pf-2-lavenganza-front-pqra5hqaa-migorriti.vercel.app/");
   } catch (error) {
     return res.status(404).json({ error: error.message });
   }
